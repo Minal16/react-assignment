34e1a969cb917da76bfef874ab0cc5b9e44a05e8
DavidC <david@origamica.com>
2016-07-07 11:45:49 +0200

    change src name and change webpack config
    added dev a production config (cleaner)
    updated cssnext (to multiple modules)
    hot module replacement working

---
+++ webpack/webpack.base.config.js
@@ -0,0 +1,62 @@
+var webpack           = require("webpack");
+var path              = require("path");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HTMLWebpackPlugin = require("html-webpack-plugin");
+
+var config = {
+  entry: [
+    path.resolve("src/index.js"),
+  ],
+
+  output: {
+    path: path.resolve("public"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+
+  resolve: {
+    extensions: ["", ".js"],
+    alias: {
+      "app": path.resolve("src"),
+    }
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve("src/index.html"),
+      filename: 'index.html', // wasnt in MZ
+      inject: 'body', // wasnt in MZ
+      minify: { collapseWhitespace: true }
+    }),
+    new ExtractTextPlugin('index.css', { allChunks: true }),
+    new CopyWebpackPlugin([{ from: 'assets' }]),
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel" },
+    ],
+
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&minimize!postcss-loader')
+      },
+      {
+        test: /\.svg$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+      },
+    ]
+  },
+
+  postcssPlugins: [
+    require('postcss-import')({ addDependencyTo: webpack }),
+    require("postcss-url")(),
+    require("postcss-cssnext")(),
+    require("postcss-nested")(),
+    require('precss'),
+  ],
+};
+
+module.exports = config;
