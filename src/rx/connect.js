71609d13431803e77cacd89fabb8f4dbb3bdcea3
DavidC <david@origamica.com>
2016-07-09 13:30:13 +0200

    cleaning

--- src/rx/connect.js
+++ src/rx/connect.js
@@ -4,12 +4,6 @@ const connect = (state$, selector = (state) => state) => (WrappedComponent) =>
     class Connect extends React.Component {
       componentWillMount() {
         this.subscription = state$.map(selector).subscribe(::this.setState);
-        // this.subscription = state$.map(selector).subscribe(rstate => {
-        //   console.log('STATE: ', rstate);
-        //   this.setState(rstate);
-        //   console.log('HERE');
-        //   console.log('STATE SET: ', this.state);
-        // });
       }
 
       componentWillUnmount() {
